a5cc04c5f14da4b390c276be01ca212b995b999a
Michael Hildebrandt <hildebrandt.mj@gmail.com>
2023-10-27 09:44:18 -0700

    Inital Commit: Direcetory Created

---
+++ main.py
@@ -0,0 +1,4 @@
+# Created By Michael Jospeh Hildebrandt. Started 10/27/2023 -> Finished
+# Timeline of creation avalible through the Git changelog.
+# This program should be run as a .exe outside of testing.
+
